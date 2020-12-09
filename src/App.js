c3365d32ceabdd5cc5e65e2fd66e3fcadfe2ba58
nenadbarjak <nenadbarjak@gmail.com>
2020-12-09 14:51:53 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>CRYPTO LIST</h1>
+        </div>
+    );
+}
+ 
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCssAssetPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+    optimization: {
+        minimize: true,
+        minimizer: [ new TerserPlugin({}), new OptimizeCssAssetPlugin({}) ]
+    },
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'public', 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'styles.css' })
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true,
+        publicPath: '/dist/'
+    }
+}
